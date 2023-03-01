ad3336d337378ff1f7df2dd19e9284f19a571eea
asylbekovatilee <asylbekovanasyikat@gmail.com>
2023-03-01 10:59:20 +0600

    shop done

--- shop/products.py
+++ shop/products.py
@@ -20,3 +20,32 @@ def read():
     цена: {product['price']}
     =============================================
     """)
+        
+def delete():
+    products = get_products()
+    print("выберите продукт для удаления: ")
+    for ind, prod in enumerate(products):
+        print(f"{ind} => {prod['title']}")
+    index = int(input())
+    products.pop(index)
+    update_products(products)
+
+def update():
+    products = get_products()
+    print("выберите продукт для обнавление: ")
+    for ind, prod in enumerate(products):
+        print(f"{ind} => {prod['title']}")
+    index = int(input())
+    prod = products[index]
+    print(f""")
+название: {prod['title']}  )
+цена: {prod['price']}
+""")
+    field = input("какое поле хотите обновить? (title, price\n")
+    value = input("введите значение для этого поля: ")
+    if field == 'title':
+        prod['title'] = value
+    elif field == 'price':
+        prod['price'] = float(value)
+    update_products(products)
+
